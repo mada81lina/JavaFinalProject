3aa0e6abc8b490483e6f2ff7e43218124ccec0a7
Sony <Sony@Lukacs>
2016-04-27 22:47:05 +0300

    second commit

--- project/src/main/java/project/Expence.java
+++ project/src/main/java/project/Expence.java
@@ -4,7 +4,7 @@ public class Expence {
 	String name;
 	Double value;
 	ExpensesType type;
-
+	//second commit
 	public Expence(String name, Double value, ExpensesType type) {
 		this.name = name;
 		this.value = value;
--- project/src/main/java/project/ExpensesType.java
+++ project/src/main/java/project/ExpensesType.java
@@ -1,5 +1,5 @@
 package project;
-
+//second commit
 public enum ExpensesType {
 	ONETIME, DAILY, WEEKLY, MONTHLY;
 
